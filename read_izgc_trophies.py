365fedb9380908f654ed368cce8ad82f5998ac21
Sarah Kiesel-Nield <query42@gmail.com>
2022-12-13 19:01:50 -0800

    Add --start-page functionality (#11)

--- read_izgc_trophies.py
+++ read_izgc_trophies.py
@@ -7,10 +7,26 @@ dispatcher = Dispatcher()
 dispatcher.login(required=False)
 club_thread = IZGCThread(dispatcher=dispatcher)
 
+
+class InvalidArgumentError(Exception):
+    pass
+
+
 # Accept command line argument to read all pages
 if "--all-pages" in sys.argv:
     club_thread.page_number = 1
     club_thread.last_post = 0
+elif sys.argv[1] == "--start-page":
+    USAGE_MESSAGE = "Usage is: read_izgc_trophies --start-page {page number}"
+    try:
+        club_thread.page_number = int(sys.argv[2])
+        club_thread.last_post = 0
+    except IndexError as exc:
+        raise InvalidArgumentError(
+            f"No page number supplied.\n{USAGE_MESSAGE}") from exc
+    except ValueError as exc:
+        raise InvalidArgumentError(
+            f"Invalid page number supplied.\n{USAGE_MESSAGE}") from exc
 
 imp_trophies = club_thread.trophy_scan()
 reporter = TrophyReporter(imp_trophies)
